38650cccaa1c25d43fa7ee2ebb091fe8becf55b8
neelbhuva <neel.7365@gmail.com>
2016-02-11 15:02:55 +0530

    naming convention addded

--- hough_circles.py
+++ hough_circles.py
@@ -3,8 +3,12 @@ import numpy as np
 import json
 #distance from left to first circle is around 13, radius is 8-9, dist btw 2 circles is 6
 #distance btw circle in first row and second row is 18
-dist_from_left = 13
 dist_btw_circles = 6
+
+def namingConvention(test_no):
+	with open('data.json') as data_file:
+		data = json.load(data_file)
+	return data["test"][test_no-1]
 
 def detectCircles(img,cimg):
 	points1 = []
@@ -37,45 +41,6 @@ def detectCircles(img,cimg):
 	cv2.imwrite("/home/neel/opencvproject/REAP/test.jpg",cimg)
 	return points1,circles
 
-'''def crop(cimg,points,circles):
-	for i in range(0,len(points)-1):
-		#print("i : " + str(i))
-		x1 = points[i][0]-points[i][2] # This line is not required
-		x2 = cimg.shape[1] # width of the image
-		y1 = points[i][1]-points[i][2]
-		y2 = points[i+1][1]
-		#check if the two circles are from two different questions and not the same question
-		if y2-y1 > points[i][2]+50 :
-			y1 = points[i-1][1]-points[i-1][2]
-			x1 = points[i-1][0]-points[i-1][2]
-			# get the region of interest i.e the answer part
-			roi = cimg[y1:y2,x1:x2]
-			cv2.imwrite("/home/neel/opencvproject/REAP/answers/"+str(part1)+part2+".jpg",roi)
-		else:
-			#circles are from the same question, get the first part of the question number
-			part1 = quesNumber(points[i][0])
-			#get the second part
-			part2 = quesNumber(points[i+1][0])
-			if part2 == 1:
-				part2 = "a"
-			elif part2 == 2:
-				part2 = "b"
-			elif part2 == 3:
-				part2 = "c"
-			elif part2 == 4:
-				part2 = "d"
-			else:
-				part2 = "e"
-			print("\npart1 : "+str(part1))
-			print("part2 : "+part2)
-			#print(points[i][0],points[i+1][0])
-
-		if i == 6:
-			y2 = cimg.shape[0] #height of the image
-			print(x1,y1,x2,y2)
-			roi = cimg[y1:y2,x1:x2]
-			cv2.imwrite("/home/neel/opencvproject/REAP/answers/"+str(part1)+part2+"1.jpg",roi)'''
-
 def crop(cimg,points,circles):
 	j = 0
 	for i in range(0,len(points)-1):
@@ -90,7 +55,7 @@ def crop(cimg,points,circles):
 			x1 = points[i-1][0]-points[i-1][2]
 			# get the region of interest i.e the answer part
 			roi = cimg[y1:y2,x1:x2]
-			cv2.imwrite("/home/neel/opencvproject/REAP/answers/"+str(part1)+part2+".jpg",roi)
+			cv2.imwrite("/home/neel/opencvproject/REAP/answers/"+test_name+sub_code+str(part1)+part2+".jpg",roi)
 		else:
 			#circles are from the same question, get the first part of the question number
 			part1 = quesNumber(circles[j:j+5],points[i][0],points[i][2])
@@ -115,7 +80,7 @@ def crop(cimg,points,circles):
 			y2 = cimg.shape[0] #height of the image
 			print(x1,y1,x2,y2)
 			roi = cimg[y1:y2,x1:x2]
-			cv2.imwrite("/home/neel/opencvproject/REAP/answers/"+str(part1)+part2+".jpg",roi)
+			cv2.imwrite("/home/neel/opencvproject/REAP/answers/"+test_name+sub_code+str(part1)+part2+".jpg",roi)
 
 def quesNumber(circles,x1,radius):
 	j = 0
@@ -139,28 +104,25 @@ def quesNumber(circles,x1,radius):
 
 
 if __name__ == '__main__':	
-	image = input("Enter the name of the image with extension : ")
-	
+	image = input("\nEnter the name of the image with extension : ")
+	test_no = int(input("\nEnter the test number (eg:1,2,3) : "))
+	sub_code = input("\nEnter the subject code(Eg:12CS306) : ")
 	#read the image
 	cimg = cv2.imread('/home/neel/opencvproject/testimages/'+image+'.jpg')
-
 	#convert to grayscale image
 	img = cv2.imread('/home/neel/opencvproject/testimages/'+image+'.jpg',0)
-
 	#get the part before the left margin
-	img = img[0:img.shape[0],0:150]
-	
+	img = img[0:img.shape[0],0:150]	
 	#img = cv2.GaussianBlur(img,(5,5),0)
 	img = cv2.medianBlur(img,5)
-	thresh = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
-	
+	thresh = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)	
+	#read the naming convention from json file
+	test_name = namingConvention(test_no)
 	#get the co-ordinates and radius of filled circles
 	points,circles = detectCircles(img,cimg)
-	
 	#crop the answer parts and also detect corresponding question numbers
 	crop(cimg,points,circles)
 	print(points)
-
 	cv2.imwrite("/home/neel/opencvproject/REAP/thresh.jpg",thresh)
 	cv2.imshow('detected circles',cimg)
 	cv2.waitKey(0)
